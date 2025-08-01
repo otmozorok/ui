e334e6673236987a3563aa8f9f930f44f58d5133
Boris Hasikov <laapl@ya.ru>
2025-08-01 13:44:35 +0300

    Обновил Avatar

--- components/avatar/template.js
+++ components/avatar/template.js
@@ -1,4 +1,4 @@
-import { COLOR, html, SHAPE } from '../.shared/index.js';
+import { COLOR, html, SHAPE, WCATTR } from '../.shared/index.js';
 
 export default html`
   <template>
@@ -30,32 +30,32 @@ export default html`
         display: none;
       }
 
-      :host([color='${COLOR.Red}']) .bg {
+      :host([${WCATTR.Color}='${COLOR.Red}']) .bg {
         background-image: var(--gradient-red);
       }
 
-      :host([color='${COLOR.Green}']) .bg {
+      :host([${WCATTR.Color}='${COLOR.Green}']) .bg {
         background-image: var(--gradient-green);
       }
 
-      :host([color='${COLOR.Orange}']) .bg {
+      :host([${WCATTR.Color}='${COLOR.Orange}']) .bg {
         background-image: var(--gradient-orange);
       }
 
-      :host([color='${COLOR.Blue}']) .bg {
+      :host([${WCATTR.Color}='${COLOR.Blue}']) .bg {
         background-image: var(--gradient-blue);
       }
 
-      :host([color='${COLOR.Purple}']) .bg {
+      :host([${WCATTR.Color}='${COLOR.Purple}']) .bg {
         background-image: var(--gradient-purple);
       }
 
-      :host([shape='${SHAPE.Circle}']) .wrapper {
+      :host([${WCATTR.Shape}='${SHAPE.Circle}']) .wrapper {
         border-radius: 50%;
         overflow: hidden;
       }
 
-      :host([shape='${SHAPE.Squircle}']) .wrapper {
+      :host([${WCATTR.Shape}='${SHAPE.Squircle}']) .wrapper {
         mask-image: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI1NiIgaGVpZ2h0PSI1NiIgZmlsbD0ibm9uZSI+PHBhdGggZmlsbD0iIzAwMCIgZD0iTTI4IDBDNC45NDIgMCAwIDQuOTQyIDAgMjhjMCAyMy4wNDYgNC45MzcgMjcuOTk1IDI3Ljk2NSAyOGguMDdjMjMuMDE2LS4wMDUgMjcuOTYtNC45NDkgMjcuOTY1LTI3Ljk2NXYtLjA3QzU1Ljk5NSA0LjkzNyA1MS4wNDYgMCAyOCAwIi8+PHBhdGggZmlsbD0iIzAwMCIgZD0iTTAgMjhDMCA0Ljk0MiA0Ljk0MiAwIDI4IDBzMjggNC45NDIgMjggMjgtNC45NDIgMjgtMjggMjhTMCA1MS4wNTggMCAyOCIvPjwvc3ZnPg==);
         mask-position: center;
         mask-repeat: no-repeat;
