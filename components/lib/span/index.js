36d64cc6ebfc9bf27ec1dd4ce2e4781255cd5fbe
Boris Hasikov <laapl@ya.ru>
2025-09-15 20:51:38 +0300

    Обновил props у span

--- components/lib/span/index.js
+++ components/lib/span/index.js
@@ -1,5 +1,5 @@
-import { appearanceProp } from '../../props/index.js';
 import { WebComponent } from '../../web-component/index.js';
+import props from './props.js';
 import template from './template.js';
 
 /**
@@ -8,6 +8,6 @@ import template from './template.js';
  */
 export class SpanComponent extends WebComponent {
   constructor() {
-    super(template, { ...appearanceProp });
+    super(template, props);
   }
 }
