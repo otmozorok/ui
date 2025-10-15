64c06e2bcbb35e46a9298c5c6fc144fa4daf6106
Boris Hasikov <laapl@ya.ru>
2025-10-15 21:51:58 +0300

    Исправил тип Button в react

--- packages/react/src/components/button/model/props.ts
+++ packages/react/src/components/button/model/props.ts
@@ -1,11 +1,10 @@
 import { APPEARANCE, MODE, SIZE } from '@otmozorok/wc/consts';
 
-export interface IButtonProps {
+export interface IButtonProps extends React.ComponentPropsWithoutRef<'button'> {
   mode?: (typeof MODE)[keyof typeof MODE];
   size?: (typeof SIZE)[keyof typeof SIZE];
   appearance?: (typeof APPEARANCE)[keyof typeof APPEARANCE];
   loading?: boolean;
   fullWidth?: boolean;
   onClick?: () => void;
-  children?: any;
 }
