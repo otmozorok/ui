f319f9301eac30bd24f89293bfdb87d4ad0becbe
Boris Hasikov <laapl@ya.ru>
2025-10-20 20:30:30 +0300

    Исправил ошибку билда для vue

--- packages/vue/src/components/button/model/props.ts
+++ packages/vue/src/components/button/model/props.ts
@@ -1,7 +1,7 @@
 import { APPEARANCE, MODE, SIZE } from '@otmozorok/wc/consts';
 import { HTMLAttributes } from 'vue';
 
-export interface IButtonProps extends HTMLAttributes {
+export interface IButtonProps extends /* @vue-ignore */ HTMLAttributes {
   size?: (typeof SIZE)[keyof typeof SIZE];
   appearance?: (typeof APPEARANCE)[keyof typeof APPEARANCE];
   mode?: (typeof MODE)[keyof typeof MODE];
--- packages/vue/src/components/headline/model/props.ts
+++ packages/vue/src/components/headline/model/props.ts
@@ -1,7 +1,7 @@
 import type { APPEARANCE, SIZE } from '@otmozorok/wc/consts';
 import { HTMLAttributes } from 'vue';
 
-export interface IHeadlineProps extends HTMLAttributes {
+export interface IHeadlineProps extends /* @vue-ignore */ HTMLAttributes {
   size?: (typeof SIZE)[keyof typeof SIZE];
   appearance?: (typeof APPEARANCE)[keyof typeof APPEARANCE];
   caps?: boolean;
